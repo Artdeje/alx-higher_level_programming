3e2b741bfcdb983b7aedbfa7d9f6a501f1e38da3
Artdeje <artdeje@gmail.com>
2023-05-11 21:27:51 +0000

    Task 102 commit error free

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-
+	"""Match bytecode provided by ALX school"""
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -11,5 +11,3 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
-
-"""Match bytecode provided by Holberton School"""
