7f1775a0c12ae20939c3434dc0622e67720b7f42
Artdeje <artdeje@gmail.com>
2023-05-11 21:31:52 +0000

    Task yananiranye hose mu Rwanda

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@
 
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-"""Match bytecode provided by ALX school"""
+    """Match bytecode provided by ALX school"""
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
