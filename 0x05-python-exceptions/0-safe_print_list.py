acb622d18a12801bfb6cfdd31806d1ea4069c9c1
Artdeje <artdeje@gmail.com>
2023-05-29 13:52:20 +0000

    Task 0 initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-def print_elements(lst, num_elements):
-    try:
-        for i in range(num_elements):
-            print(lst[i])
-    except IndexError:
-        print("Index out of range!")
-
-# Down codes are printing x list
-#Return: x > my_list
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
-x = 15
-
-print_elements(my_list, x)
+def safe_print_list(my_list=[], x=0):
+    index = 0
+    while True:
+        try:
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
+        except IndexError:
+            print()
+            return index
