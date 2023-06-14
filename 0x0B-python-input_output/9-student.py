59abbe15d8d0d5d5913ae7b7217b2c1a4f231525
Artdeje <artdeje@gmail.com>
2023-06-14 22:49:10 +0000

    Task 9 correct commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,20 +3,20 @@
 
 
 class Student:
-    """Represent the student."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initialize new Student.
 
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): The first name of student.
+            last_name (str): The last name of student.
+            age (int): The age of student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of Student."""
+        """Get the dictionary representation of Student."""
         return self.__dict__
