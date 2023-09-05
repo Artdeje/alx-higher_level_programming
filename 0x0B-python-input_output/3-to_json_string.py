a540a75ba68c711b8c50380ca992de0a0eff1ac9
Artdeje <122992855+Artdeje@users.noreply.github.com>
2023-09-05 23:07:36 +0200

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines the string-to-JSON function."""
-import json
-
-
-def to_json_string(my_obj):
-    """Return the JSON representation of string object."""
-    return json.dumps(my_obj)
