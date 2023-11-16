8ecea5ad5304ceb5a26ef9e83886fc4632c2a10d
Artdeje <artdeje@gmail.com>
2023-11-15 16:03:06 -0800

    correct

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let msg1 = "C is fun";
-let msg2 = "Python is cool";
-let msg3 = "JavaScript is amazing";
+let msg1 = 'C is fun';
+let msg2 = 'Python is cool';
+let msg3 = 'JavaScript is amazing';
 console.log( msg1 );
 console.log( msg2 );
 console.log( msg3 );
